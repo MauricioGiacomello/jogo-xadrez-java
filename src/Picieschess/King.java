ff585301a57301c97aff0d2ada6ba17114ea4854
MauricioGiacomello <giacomerro@gmail.com>
2020-10-21 11:33:05 -0300

    Adicionei a logica da movimentação do rei

--- src/Picieschess/King.java
+++ src/Picieschess/King.java
@@ -1,6 +1,7 @@
 package Picieschess;
 
 import boordGame.Board;
+import boordGame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,10 +16,80 @@ public class King extends ChessPiece {
         return "K";
     }
 
-    //Implementação básica para acessar 
+    //Verifica se a posição existe e se a posição na qual a peça vai ser movida é uma peça adversaria//
+    private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
+	}
+
     @Override
     public boolean[][] possibleMove() {
+
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-        return mat;
+
+        Position p = new Position(0, 0);
+
+    /************************************* ACIMA DA PEÇA *****************************************/
+
+    p.setValues(position.getRow() - 1, position.getColumn());
+
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+    /************************************* ABAIXO DA PEÇA *****************************************/
+
+    p.setValues(position.getRow() + 1, position.getColumn());
+
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
     }
+
+    /************************************* ESQUERDA DA PEÇA ***************************************/
+
+    p.setValues(position.getRow(), position.getColumn() - 1);
+        
+        if(getBoard().positionExists(p) && canMove(p)){
+        mat[p.getRow()][p.getColumn()] = true;
+    }
+
+    /************************************* DIREITA DA PEÇA ***************************************/
+
+    p.setValues(position.getRow(), position.getColumn() + 1);
+    if(getBoard().positionExists(p) && canMove(p)){
+        mat[p.getRow()][p.getColumn()] = true;
+    }
+
+    /************************************* NOROESTE DA PEÇA ***************************************/
+
+    p.setValues(position.getRow() - 1, position.getColumn() - 1);
+    if(getBoard().positionExists(p) && canMove(p)){
+        mat[p.getRow()][p.getColumn()] = true;
+    }
+
+    /************************************* NOROESTE DA PEÇA ***************************************/
+
+    p.setValues(position.getRow() - 1, position.getColumn() + 1);
+    if(getBoard().positionExists(p) && canMove(p)){
+        mat[p.getRow()][p.getColumn()] = true;
+    }
+
+     /************************************* SULDOESTE DA PEÇA ***************************************/
+
+     p.setValues(position.getRow() + 1, position.getColumn() - 1);
+     if(getBoard().positionExists(p) && canMove(p)){
+         mat[p.getRow()][p.getColumn()] = true;
+     }
+
+     /************************************* SULDESTE DA PEÇA ***************************************/
+
+     p.setValues(position.getRow() + 1, position.getColumn() + 1);
+     if(getBoard().positionExists(p) && canMove(p)){
+         mat[p.getRow()][p.getColumn()] = true;
+     }
+
+     return mat;
+     
+    }
+
 }
--- src/Picieschess/Rook.java
+++ src/Picieschess/Rook.java
@@ -8,6 +8,7 @@ import chess.ChessPiece;
 import chess.Color;
 
 public class Rook extends ChessPiece {
+    
     public Rook(Board board, Color color) {
         super(board, color);
     }
